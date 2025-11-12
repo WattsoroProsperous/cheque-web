4796bbb07594e9e14980fabab2eb30c445e0111d
WattsoroProsperous <watantcha724@gmail.com>
2025-11-12 16:53:15 +0000

    Mainsou ESN 2.0.7

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2367626320"
+    serviceWorkerVersion: "3242810312"
   }
 });
